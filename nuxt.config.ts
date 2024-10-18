82d4e8ab10fd5d6c5b98da41f78ef5e3a5301073
Martin Masevski <martin.masevski@gmail.com>
2024-10-18 15:31:14 +0200

    Add nuxt-headlessui module, update nuxt version, and add tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-headlessui']
+  modules: ['nuxt-headlessui', '@nuxtjs/tailwindcss'],
 })
